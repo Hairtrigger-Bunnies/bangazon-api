051ef14b02f60b703c6d89ad1dcd9783e2d823b4
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-09-12 11:50:36 -0500

    changed route from bangazon to bangazon-api

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(middleware);
 
 
 let routes = require('./Routes/');
-app.use(`/bangazon/`, routes);
+app.use(`/bangazon-api/`, routes);
 
 app.use( (req, res, next) => {
   let err = new Error('Not Found, dummy');
