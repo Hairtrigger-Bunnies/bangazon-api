f8f6e79a43fe1903fbc7831f1b88b889df222262
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-09-12 10:59:50 -0500

    edited gitignore, added app.js file, worked on controller model and route for task3... going to log and check now!

--- Controllers/ProductsCtrll.js
+++ Controllers/ProductsCtrll.js
@@ -7,3 +7,22 @@
 // DELETE
 // User should be able to GET a list of products, and GET a single product.
 
+'use strict';
+
+const { getAll } = require('../models/Products');
+
+module.exports.getProducts = (req, res, next) => {
+  getAll()
+  .then( (products) => {
+    res.status(200).json(products);
+  })
+  .catch( (err) => next(err));
+};
+
+module.exports.getOneProduct = ({params: {id}}, res, next) => {
+  // req.params.id
+  db.get(`SELECT * FROM products WHERE product_id = ${id}`, (err, products) => {
+    if (err) return next(err);
+    res.status(200).json(products);
+  });
+};
--- Model/Products.js
+++ Model/Products.js
@@ -6,3 +6,18 @@
 // PUT
 // DELETE
 // User should be able to GET a list of products, and GET a single product.
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/mediaStore.sqlite');
+
+module.exports = {
+  getAll: () => {
+      console.log("all products");
+    // return new Promise( (resolve, reject) => {
+    //   db.all(`SELECT * FROM products`, (err, product) => {
+    //     if (err) return reject(err);
+    //     resolve(product);
+    //   });
+    // });
+  }
+}
