f972e335f81ddac01a1c9a1b7dd89153439ceb5c
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-09-12 10:49:40 -0500

    work on product type controller

---
+++ Controllers/ProductTypeCtrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Below is the requirements this controller will handle:
+// Verbs to be supported:
+// GET
+// POST
+// PUT
+// DELETE
+// User should be able to GET a list, and GET a single item.
+
