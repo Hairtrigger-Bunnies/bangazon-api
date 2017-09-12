fddecba266e210842e1165c30177c0c4ccd5a4c2
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-09-12 14:08:39 -0500

    work on testing files and npm install nodemon

--- Model/ProductType.js
+++ Model/ProductType.js
@@ -13,6 +13,11 @@ const sqlite3 = require('sqlite3').verbose();
 // creates a database that can be accessed within sqlite
 const db = new sqlite3.Database('./db/mediaStore.sqlite');
 
+// const path = require('path')
+// const dbPath = path.resolve(__dirname, 'todo.db')
+// const db = new sqlite3.Database(dbPath)
+
+
 // exports the result of a promse
 module.exports = {
   // sets getAll to equal the result of an anonymous function
@@ -20,11 +25,11 @@ module.exports = {
     // returns a new promise that will either resolve or reject
     return new Promise( (resolve, reject) => {
       // runs the method all on the variable db and selects all data within the db. Then it makes the data available to be executed in the controller
-      db.all(`SELECT * FROM Product_Type`, (err, Data) => {
+      db.all(`SELECT * FROM Product_Type`, (err, prodData) => {
         // if there's an error, it rejects and displays an error within the console
         if(err) return reject(err);
         // if no error, it resolves and returns the data
-        resolve(Data);
+        resolve(prodData);
       });
     });
   }
