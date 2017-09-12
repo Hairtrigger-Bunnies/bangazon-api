9185da8d909410fa87175a87bb400943da738ebb
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-09-12 15:58:17 -0500

    update product controller to run correctly

--- Controllers/ProductTypeCtrl.js
+++ Controllers/ProductTypeCtrl.js
@@ -9,17 +9,25 @@
 // User should be able to GET a list, and GET a single item.
 
 // seting an object containing getAll to run product types within model folder
-const { getAll } = require('../Model/ProductType');
+const { getAll, getOne } = require('../Model/ProductType');
 
 // exporting the result of an anonymous function within getProductType while expecting require, response, and next to be passed in
-module.exports.getProductType = (req, res, next) => {
+module.exports.getProductTypes = (req, res, next) => {
   // executes the function getAll
   getAll()
   // then runs an anonymous function while expecting prodType to be passed in
-  .then( (prodType) => {
+  .then( (direx) => {
     // shows response status 200 on the json file relating to prodType
-    res.status(200).json(prodType);
+    res.status(200).json(direx);
   })
   // if there's an error, it catches and runs the error message
+  .catch( (err) => next(err));
+};
+
+module.exports.getProductType = ({params: {id}}, res, next) => {
+  getOne(id)
+  .then( (direx) => {
+    res.status(200).json(direx);
+  })
   .catch( (err) => next(err));
 };
