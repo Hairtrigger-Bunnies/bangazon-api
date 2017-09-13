e437ed7900114b71e55a98226ee61b0a18dbce1e
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-09-13 09:23:19 -0500

    touch up files and add requirements from project page to the top of each file

--- Routes/ProductType.js
+++ Routes/ProductType.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// Below is the requirements this controller will handle:
+// Verbs to be supported:
+// GET
+// POST
+// PUT
+// DELETE
+// User should be able to GET a list, and GET a single item.
+
 // requires in express to the object Router
 const { Router } = require('express');
 // sets router to equal Router and then gets executed
