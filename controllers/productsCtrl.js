d510b00b7e2a8730bf332fd0e2744111120702a9
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-09-15 09:01:16 -0500

    shell of post put delete products done needs to be tested

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { getAllProducts, getSingleProduct } = require('../models/Products');
+const { getAllProducts, getSingleProduct, postOneProduct, putOneProduct, deleteOneProduct } = require('../models/Products');
 
 module.exports.getProducts = (req, res, next) => {
   getAllProducts()
@@ -12,6 +12,31 @@ module.exports.getProducts = (req, res, next) => {
 
 module.exports.getOneProduct = ({params: {id}}, res, next) => {
   getSingleProduct(id)
+  .then( (products) => {
+    res.status(200).json(products);
+  })
+  .catch( (err) => next(err));
+};
+
+module.exports.postNewProduct = (req, res, next) => {
+  console.log("from ctrl", req.body);
+  postOneProduct(req.body)
+  .then( (data) => {
+    res.status(200).json(data);
+  })
+  .catch( (err) => next(err));
+};
+
+module.exports.putNewProduct = (req, res, next) => {
+  putOneProduct(req.body)
+  .then( (data) => {
+    res.status(200).json(data);
+  })
+  .catch( (err) => next(err));
+};
+
+module.exports.deleteOneProduct = ({params: {id}}, res, next) => {
+  deleteSingleProduct(id)
   .then( (products) => {
     res.status(200).json(products);
   })
