ce1373fa1f1c9e751bb077cbd91db0ce74e5a5f1
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-09-15 15:15:58 -0500

    made sure that the order date can only be between the customers account being created and the customers last login

--- data/orders.js
+++ data/orders.js
@@ -3,13 +3,27 @@
 
 const faker = require('faker');
 
-module.exports.generateOrders = (payTypesLen, customersLen) => {
+module.exports.generateOrders = (payTypesLen, customers) => {
   let orders = [];
 
   for (let i = 0; i < 25; i++) {
-    let order_date = faker.date.recent().toISOString();
+
+    //MAKE SURE ORDERS CAN ONLY BE PLACED BETWEEN THE TIME A CUSTOMER IS CREATED AND LAST LOGGED ON
+    function formatDate(data) {
+      var date = new Date(data),
+          month = '' + (date.getMonth() + 1),
+          day = '' + date.getDate(),
+          year = date.getFullYear();
+  
+      if (month.length < 2) month = '0' + month;
+      if (day.length < 2) day = '0' + day;
+  
+      return [year, month, day].join('-');
+    }
+
+    let order_date = faker.date.between(formatDate(customers[i].past_date), formatDate(customers[i].date_range)).toISOString();
     let payment_type_id = Math.floor(Math.random() * payTypesLen) + 1;
-    let customer_id = Math.floor(Math.random() * customersLen) + 1;
+    let customer_id = Math.floor(Math.random() * customers.length) + 1;
 
     orders.push({
       order_date,
